aae02eda51afeb268fa6dd26e82a77b068a07a39
AvinashMSangle <avinashMS.3119@proton.me>
2024-12-04 07:51:33 +0000

    preview component complete

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import configs from "@/app/assets/configs.json";
+console.log(configs);
 
-function Preview() {
+
+function Preview({
+   color = "Black Pearl",
+   interior = "dark",
+   wheel="SlipStream Black"
+}) {
+
   return (
     <>
       {/* EXTERIOR IMAGE*/}
       <Image
-        src={"/images/fiskerCar.jpg"}
+        src={configs[color].wheels[wheel]}
         width="0"
         height="0"
         sizes="100vw"
@@ -15,12 +23,12 @@ function Preview() {
       {/* DESCCRIPTION */}
       <div className="py-3">
         <h3 className="font-bold text-xl text-blue-950">Metallic Gloss</h3>
-        <p>A mysterious, seductive black with a metallic blue shimmer.</p>
+        <p>{configs[color].description}</p>
       </div>
 
       {/* Interior Image */}
       <Image
-        src={"/images/dark.jpeg"}
+        src={`/images/interior/${interior}.jpeg`}
         width="0"
         height="0"
         sizes="100vw"
